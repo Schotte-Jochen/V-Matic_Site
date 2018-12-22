6e482e2002f5aa155faedca2236347236d96288c
Jochen Schotte <jochen.schotte@student.vives.be>
2018-12-22 17:54:18 +0100

    Verdere implementatie mail (nog steeds onsuccesvol)

--- V-Matic_Site/Controllers/HomeController.cs
+++ V-Matic_Site/Controllers/HomeController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Net.Mail;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Vmatic_Site.Models;
@@ -33,7 +36,7 @@ namespace V_Matic_Site.Controllers
             //TO-DO: Mail-service in werking krijgen
             if (ModelState.IsValid)
             {
-                try
+                /*try
                 {
                     MailMessage mail = new MailMessage();
                     mail.From = new MailAddress(content.Sender);
@@ -42,9 +45,9 @@ namespace V_Matic_Site.Controllers
                     mail.Body = content.Message;
 
                     SmtpClient smtp = new SmtpClient();
-                    smtp.Host = "smtp.one.com";
+                    smtp.Host = "send.one.com";
                     smtp.Port = 465;
-                    smtp.Credentials = new System.Net.NetworkCredential("beheerder@vmatic.be", "vmatic47");
+                    smtp.Credentials = new System.Net.NetworkCredential("dummy@vmatic.be", "dummy1");
                     smtp.EnableSsl = true;
                     smtp.Send(mail);
                     ModelState.Clear();
@@ -55,12 +58,96 @@ namespace V_Matic_Site.Controllers
                 {
                     ModelState.Clear();
                     return View("Error");
-                }
+                }*/
+                //prepare email
+                var toAddress = "beheerder@vmatic.be";
+                var fromAddress = content.Sender;
+                var subject = content.Subject;
+                var message = content.Message;
+
+                //start email Thread
+                var tEmail = new Thread(() =>
+               SendEmail(toAddress, fromAddress, subject, message));
+                tEmail.Start();
+                return View("Thanks");
             }
             else
             {
                 return View();
             }
+        }
+
+        private void SendEmail(string toAddress, string fromAddress,
+                      string subject, string message)
+        {
+            try
+            {
+                using (var mail = new MailMessage())
+                {
+                    const string email = "dummy@telenet.be";
+                    const string password = "dummy1";
+
+                    var loginInfo = new NetworkCredential(email, password);
+
+
+                    mail.From = new MailAddress(fromAddress);
+                    mail.To.Add(new MailAddress(toAddress));
+                    mail.Subject = subject;
+                    mail.Body = message;
+                    mail.IsBodyHtml = true;
+
+                    try
+                    {
+                        using (var smtpClient = new SmtpClient(
+                                                         "send.one.com", 465))
+                        {
+                            smtpClient.EnableSsl = true;
+                            smtpClient.UseDefaultCredentials = false;
+                            smtpClient.Credentials = loginInfo;
+                            smtpClient.Send(mail);
+                        }
+
+                    }
+
+                    finally
+                    {
+                        //dispose the client
+                        mail.Dispose();
+                    }
+
+                }
+            }
+            catch (SmtpFailedRecipientsException ex)
+            {
+                /*foreach (SmtpFailedRecipientException t in ex.InnerExceptions)
+                {
+                    var status = t.StatusCode;
+                    if (status == SmtpStatusCode.MailboxBusy ||
+                        status == SmtpStatusCode.MailboxUnavailable)
+                    {
+                        Response.Write("Delivery failed - retrying in 5 seconds.");
+                        System.Threading.Thread.Sleep(5000);
+                        //resend
+                        //smtpClient.Send(message);
+                    }
+                    else
+                    {
+                        Response.Write("Failed to deliver message to {0}",
+                                          t.FailedRecipient);
+                    }
+                }*/
+            }
+            catch (SmtpException Se)
+            {
+                // handle exception here
+                //Response.Write(Se.ToString());
+            }
+
+            catch (Exception ex)
+            {
+                //Response.Write(ex.ToString());
+            }
+
         }
 
         public IActionResult Thanks()
